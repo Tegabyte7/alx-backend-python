dbd8a5afd6468748c17657840339b94b3136e67c
Otega Otite <otegaotite@gmail.com>
2025-09-21 21:13:28 +0100

    parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,17 +5,17 @@ import utils
 
 class TestAccessNestedMap(unittest.TestCase):
 
-    @parameterized.expand([
-        ({"a": 1}, ("a"), 1),
-        ({"a": {"b": 2}}, ("a"), {'b':2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
-    ])
-    def test_access_nested_map(self, nested_map, path, expected_result):
-        self.assertEqual(utils.access_nested_map(nested_map, path), expected_result)
+    # @parameterized.expand([
+    #     ({"a": 1}, ("a"), 1),
+    #     ({"a": {"b": 2}}, ("a"), {'b':2}),
+    #     ({"a": {"b": 2}}, ("a", "b"), 2)
+    # ])
+    # def test_access_nested_map(self, nested_map, path, expected_result):
+    #     self.assertEqual(utils.access_nested_map(nested_map, path), expected_result)
 
     @parameterized.expand([
-        ({}, ('a'),),
-        ({"a": 1}, ("a", "b"),)
+        ({}, ('a'), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
     ])
     def test_access_nested_map_exception(self, nested_map, path, expected_result):
         with self.assertRaises(KeyError):
