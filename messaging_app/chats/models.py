1c0a5f2d2436452619f9d7704e9877ed6077f0d5
Otega Otite <otegaotite@gmail.com>
2025-09-28 23:12:18 +0100

    define data model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 import uuid
+from django.contrib.auth.models import AbstractUser
+
 
 
 # Create your models here.
-class Users(models.Model):
+class Users(AbstractUser):
     user_id = models.UUIDField(primary_key=True, 
                                default=uuid.uuid4,
                                editable=False,
                                unique=True, 
                                db_index=True)
-    first_name = models.CharField(max_length=70, null=False)
-    last_name = models.CharField(max_length=70, null=False)
     email = models.EmailField(max_length=70, unique=True, null=False)
     password_hash = models.CharField(max_length=120, null=False)
     phone_number = models.CharField(max_length=20)
@@ -20,6 +20,14 @@ class Users(models.Model):
                             null=True,
                             blank=True,)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return f"{self.email} ({self.role})"
+    
+
 
 class Message(models.Model):
     message_id = models.UUIDField(primary_key=True, 
@@ -37,3 +45,5 @@ class Conversation(models.Model):
                                         db_index=True)
     participants_id = models.ForeignKey(Users.user_id, on_delete=models.CASCADE)
     created_at=models.DateTimeField(auto_now_add=True)
+
+
